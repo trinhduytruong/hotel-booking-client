282280a335e44a864215ab47ee66c5b39abc2e87
TRUONG <trinhduytruong02@gmail.com>
2024-05-18 11:16:51 +0700

    add addRoom page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import AddRoom from './components/room/AddRoom'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+
+function App() {
+
+  return (
+    <div>
+      <AddRoom/>
+    </div>
+  )
+}
+
+export default App
